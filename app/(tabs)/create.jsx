338aca1ee7d5637d9176c496112940638f08131c
James Luu <jamesluu0917@gmail.com>
2024-06-15 15:32:16 -0400

    updated page to follow figma

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -8,51 +8,26 @@ import {
   ScrollView,
   ImageBackground,
 } from "react-native";
+import CheckBox from "expo-checkbox";
+import Picker from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 const Create = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("Design");
   const [date, setDate] = useState(null); // Initial state as null
-  const [startTime, setStartTime] = useState(null); // Initial state as null
-  const [endTime, setEndTime] = useState(null); // Initial state as null
   const [showDatePicker, setShowDatePicker] = useState(false);
-  const [showStartTimePicker, setShowStartTimePicker] = useState(false);
-  const [showEndTimePicker, setShowEndTimePicker] = useState(false);
-
-  const categories = [
-    "Design",
-    "Meeting",
-    "Coding",
-    "BOE",
-    "Testing",
-    "Quick Call",
-  ];
+  const [recurring, setRecurring] = useState(false);
+  const [recurrenceOption, setRecurrenceOption] = useState("Annually");
+  const [recurrencePattern, setRecurrencePattern] = useState("Pattern 1");
+  const [status, setStatus] = useState("Not Started");
+  const [assignees, setAssignees] = useState(["Jxluu"]);
 
   const handleDateChange = (event, selectedDate) => {
     setShowDatePicker(false);
     if (selectedDate) {
       setDate(selectedDate);
     }
-  };
-
-  const handleStartTimeChange = (event, selectedDate) => {
-    setShowStartTimePicker(false);
-    if (selectedDate) {
-      setStartTime(selectedDate);
-    }
-  };
-
-  const handleEndTimeChange = (event, selectedDate) => {
-    setShowEndTimePicker(false);
-    if (selectedDate) {
-      setEndTime(selectedDate);
-    }
-  };
-
-  const handleCategoryPress = (selectedCategory) => {
-    setCategory(selectedCategory);
   };
 
   return (
@@ -93,42 +68,33 @@ const Create = () => {
           </View>
         </View>
         <View style={styles.form}>
-          <View style={styles.timeSection}>
-            <View style={styles.inputGroup}>
-              <Text style={styles.label}>Start Time</Text>
-              <TouchableOpacity onPress={() => setShowStartTimePicker(true)}>
-                <Text style={styles.timeText}>
-                  {startTime ? startTime.toLocaleTimeString() : ""}
-                </Text>
-              </TouchableOpacity>
-              {showStartTimePicker && (
-                <DateTimePicker
-                  value={startTime || new Date()}
-                  mode="time"
-                  display="default"
-                  onChange={handleStartTimeChange}
-                />
-              )}
-            </View>
-            <View style={styles.inputGroup}>
-              <Text style={styles.label}>End Time</Text>
-              <TouchableOpacity onPress={() => setShowEndTimePicker(true)}>
-                <Text style={styles.timeText}>
-                  {endTime ? endTime.toLocaleTimeString() : ""}
-                </Text>
-              </TouchableOpacity>
-              {showEndTimePicker && (
-                <DateTimePicker
-                  value={endTime || new Date()}
-                  mode="time"
-                  display="default"
-                  onChange={handleEndTimeChange}
-                />
-              )}
-            </View>
-          </View>
+          <View />
           <View style={styles.inputGroup}>
-            <Text style={styles.label}>Description</Text>
+            <Text style={styles.label}>Recurring QuestBit?</Text>
+            <CheckBox
+              value={recurring}
+              onValueChange={setRecurring}
+              style={styles.checkbox}
+            />
+          </View>
+          {recurring && (
+            <>
+              <View style={styles.inputGroup}>
+                <Text style={styles.label}>Recurrence Option</Text>
+                <Picker
+                  selectedValue={recurrenceOption}
+                  style={styles.picker}
+                  onValueChange={(itemValue) => setRecurrenceOption(itemValue)}
+                >
+                  <Picker.Item label="Annually" value="Annually" />
+                  <Picker.Item label="Monthly" value="Monthly" />
+                  <Picker.Item label="Weekly" value="Weekly" />
+                </Picker>
+              </View>
+            </>
+          )}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Description *</Text>
             <TextInput
               style={[styles.input, { height: 100 }]}
               placeholder=""
@@ -137,22 +103,29 @@ const Create = () => {
               multiline
             />
           </View>
-          <View style={styles.categoryContainer}>
-            {categories.map((cat) => (
-              <TouchableOpacity
-                key={cat}
-                style={[
-                  styles.categoryButton,
-                  category === cat && styles.selectedCategory,
-                ]}
-                onPress={() => handleCategoryPress(cat)}
-              >
-                <Text style={styles.categoryText}>{cat}</Text>
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Status</Text>
+            <View style={styles.status}>
+              <Text>{status}</Text>
+            </View>
+          </View>
+          <View style={styles.inputGroup}>
+            <View style={styles.assigneesRow}>
+              <Text style={styles.label}>Assignee(s)</Text>
+              <TouchableOpacity style={styles.addAssigneeButton}>
+                <Text style={styles.addAssigneeText}>+</Text>
               </TouchableOpacity>
-            ))}
+            </View>
+            <View style={styles.assignees}>
+              {assignees.map((assignee, index) => (
+                <View key={index} style={styles.assignee}>
+                  <Text>{assignee}</Text>
+                </View>
+              ))}
+            </View>
           </View>
           <TouchableOpacity style={styles.createButton}>
-            <Text style={styles.createButtonText}>CREATE TASK</Text>
+            <Text style={styles.createButtonText}>CREATE QUESTBIT</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
@@ -196,9 +169,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 0,
+    elevation: 5,
+    zIndex: 0,
   },
   inputGroup: {
+    marginBottom: 20,
+  },
+  inputGroupRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
   },
   label: {
@@ -217,36 +197,45 @@ const styles = StyleSheet.create({
     borderBottomColor: "#ccc",
     paddingVertical: 4,
   },
-  timeSection: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+  picker: {
+    height: 50,
+    width: 150,
   },
-  timeText: {
-    fontSize: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-    paddingVertical: 4,
-  },
-  categoryContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    marginBottom: 20,
-  },
-  categoryButton: {
+  status: {
     padding: 10,
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: "#87ceeb",
-    marginBottom: 10,
-    width: "30%",
+    borderColor: "#ccc",
+    backgroundColor: "#f0f0f0",
     alignItems: "center",
   },
-  selectedCategory: {
-    backgroundColor: "#87ceeb",
+  assigneesRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  categoryText: {
-    color: "#000",
+  assignees: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 10,
+  },
+  assignee: {
+    backgroundColor: "#ccc",
+    borderRadius: 15,
+    padding: 10,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  addAssigneeButton: {
+    backgroundColor: "#87ceeb",
+    borderRadius: 15,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addAssigneeText: {
+    color: "#fff",
+    fontSize: 18,
   },
   createButton: {
     backgroundColor: "#8fbc8f",
@@ -264,6 +253,9 @@ const styles = StyleSheet.create({
   whiteTextInput: {
     borderBottomColor: "#fff",
     color: "#fff",
+  },
+  checkbox: {
+    alignSelf: "center",
   },
 });
 
