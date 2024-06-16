b0be90da59e302cbe5a7a483eb21a21dd550f29d
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-06-16 19:03:29 -0400

    Finish calendar, add buttons, adjust home page

--- components/QuestBit.jsx
+++ components/QuestBit.jsx
@@ -1,7 +1,8 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 import PixelButton from "./PixelButton";
-import Icon from "react-native-vector-icons/FontAwesome"; // Adjust the icon based on the package you're using
+import Icon from "react-native-vector-icons/FontAwesome";
+import { globalStyles } from '../app/global_css';
 
 const QuestBit = ({ item }) => {
   const dateString = item.dueDates[0];
@@ -11,14 +12,13 @@ const QuestBit = ({ item }) => {
   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
 
   return (
-    <TouchableOpacity className="bg-white rounded-xl flex-row items-center justify-between px-2 shadow-xl my-2 mx-5">
+    <TouchableOpacity style={globalStyles.border} className="bg-white rounded-xl flex-row items-center justify-between p-2 shadow-xl my-2 mx-5">
       <View className="flex-col">
         <Text className="font-zcool text-2xl text-justify">{item.title}</Text>
-        <Text className="font-zcool text-md text-gray">{item.description}</Text>
+        <Text className="font-zcool text-md text-gray my-3">{item.description}</Text>
         <View className="flex-row justify-between w-[80%] items-center">
           <PixelButton
             text={`${item.status}`}
-            imageStyle="w-[18vw]"
             textStyle="text-sm"
           />
           <View className="flex-row items-center">
