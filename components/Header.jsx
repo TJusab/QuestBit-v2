904d2627e4a2710c948af8a0449e05017d58c745
Tayba <tayba.jusab25@gmail.com>
2024-06-27 16:28:43 -0400

    Add assignees to questbits

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,14 @@
 import { Text, View, Image } from "react-native";
+import { getCurrentUser } from "../lib/account";
+import { useGlobalContext } from "../context/GlobalProvider";
+import { getUserIcon } from "../lib/icon";
 
 const Header = () => {
+  const { user } = useGlobalContext();
   return (
     <View className="flex-row justify-end mt-5">
       <Image
-        source={require("../assets/HD/character_48X48.png")}
+        source={getUserIcon(user.icon)}
         className="w-[70] h-[70] mx-5 mb-5"
       />
     </View>
