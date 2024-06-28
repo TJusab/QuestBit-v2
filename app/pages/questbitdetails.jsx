6130e990820342a50c73ea1cd8f68f64fe825c55
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-06-27 20:50:36 -0400

    adding routing and fixing css

--- app/pages/questbitdetails.jsx
+++ app/pages/questbitdetails.jsx
@@ -1,68 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
-  FlatList,
-  ActivityIndicator,
-  Alert,
   Image,
-  StyleSheet,
   TextInput,
   TouchableOpacity,
+  StyleSheet
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
-import { router, useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
+import { router } from "expo-router";
 
 const QuestBitDetails = () => {
   const { questbit } = useLocalSearchParams();
-  const item = JSON.parse(questbit);
-  const [loading, setLoading] = useState(false);
+  const item = questbit ? JSON.parse(questbit) : null;
   const [isEditing, setIsEditing] = useState(false);
 
   const toggleEditing = () => {
     setIsEditing(!isEditing);
   };
 
+  const back = () => {
+    router.back();
+  }
+
   const handleRecurrenceChange = (value) => {
-    // Implement logic to handle recurrence change
     console.log("Recurrence changed:", value);
   };
 
   const renderQuestBitDetails = () => (
     <View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingRight: 20,
-        }}
-      >
-        <View style={styles.section}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", paddingRight: 20 }}>
+        <View>
           <Text style={styles.label}>Title</Text>
           <Text style={styles.title}>{item.title}</Text>
         </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20}}>
-        <View style={styles.section}>
+        <View>
           <Text style={styles.label}>Due Date</Text>
-          <View style={styles.row}>
+          <View>
             <MaterialIcons name="event" size={20} color="black" />
-            <Text style={styles.value}>
-              {item.dueDates[0].substring(0, 10)}
-            </Text>
+            <Text>{item.dueDates[0].substring(0, 10)}</Text>
           </View>
         </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Recurrence</Text>
-          <Text style={styles.value}>Annually</Text>
-        </View>
       </View>
-      <View style={styles.section}>
-          <Text style={styles.label}>Status</Text>
-          <View style={styles.status}></View>
-        </View>
-      <View style={styles.section}>
+      <View>
+        <Text style={styles.label}>Recurrence</Text>
+        <Text style={styles.value}>Annually</Text>
+      </View>
+      <View>
+        <Text style={styles.label}>Status</Text>
+      </View>
+      <View>
         <Text style={styles.label}>Description</Text>
         <Text style={styles.value}>{item.description}</Text>
       </View>
@@ -70,7 +60,7 @@ const QuestBitDetails = () => {
         <Text style={styles.label}>Assignee(s)</Text>
         <View style={styles.row}>
           {item.assignees.map((assignee) => (
-            <View key={assignee.id} style={styles.assignee}>
+            <View key={assignee.id}>
               <Image
                 source={require("../../assets/HD/character_48X48.png")}
                 style={styles.character}
@@ -81,137 +71,104 @@ const QuestBitDetails = () => {
           ))}
         </View>
       </View>
-      <View style={styles.section}>
-        <Text style={styles.label}>QuestBit Diary</Text>
-      </View>
-      <View style={styles.log}>
-        <Image
-          source={require("../../assets/HD/scroll_small.png")}
-          style={styles.scroll}
-          resizeMethod="stretch"
-        />
+      <View>
+      <Text style={styles.label}>QuestBit Diary</Text>
+        <View style={styles.log}>
+          <Image
+            source={require("../../assets/HD/scroll_small.png")}
+            style={styles.scroll}
+            resizeMethod="stretch"
+          />
+        </View>
       </View>
     </View>
   );
 
-  const EditableQuestItem = ({ item }) => {
-    const [title, setTitle] = useState(item.title);
-    const [status, setStatus] = useState(item.status);
-    const [recurrence, setRecurrence] = useState(item.recurrence);
-    const [description, setDescription] = useState(item.description);
-
-    return (
-      <View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingRight: 20,
-          }}
-        >
-          <View style={styles.section}>
-            <Text style={styles.label}>Title</Text>
-            <TextInput
-              style={styles.title}
-              value={title}
-              onChangeText={setTitle}
-            />
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.label}>Status</Text>
-            <TextInput
-              style={styles.input}
-              value={status}
-              onChangeText={setStatus}
-            />
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingRight: 20,
-          }}
-        >
-          <View style={styles.section}>
-            <Text style={styles.label}>Due Date</Text>
-            <View style={styles.row}>
-              <MaterialIcons name="event" size={20} color="black" />
-              <Text style={styles.value}>
-                {item.dueDates[0].substring(0, 10)}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.label}>Recurrence</Text>
-            <Picker
-              style={styles.picker}
-              selectedValue={recurrence}
-              onValueChange={handleRecurrenceChange}
-            >
-              <Picker.Item label="None" value="none" />
-              <Picker.Item label="Annually" value="annually" />
-              <Picker.Item label="Monthly" value="monthly" />
-            </Picker>
-          </View>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Description</Text>
+  const EditableQuestItem = () => (
+    <View>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", paddingRight: 20 }}>
+        <View>
+          <Text>Title</Text>
           <TextInput
-            style={[styles.input, { height: 100 }]}
-            multiline
-            value={description}
-            onChangeText={setDescription}
+            value={item.title}
+            onChangeText={(text) => {}}
           />
         </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Assignee(s)</Text>
-          <View style={styles.row}>
-            {item.assignees.map((assignee) => (
-              <View key={assignee.id} style={styles.assignee}>
-                <Image
-                  source={require("../../assets/HD/character_48X48.png")}
-                  style={styles.character}
-                  resizeMode="stretch"
-                />
-                <Text style={styles.username}>{assignee.username}</Text>
-              </View>
-            ))}
-          </View>
+        <View>
+          <Text>Status</Text>
+          <TextInput
+            value={item.status}
+            onChangeText={(text) => {}}
+          />
         </View>
       </View>
-    );
-  };
-
-  if (loading) {
-    return (
-      <View style={styles.centered}>
-        <ActivityIndicator size="large" />
+      <View style={{ flexDirection: "row", justifyContent: "space-between", paddingRight: 20 }}>
+        <View>
+          <Text>Due Date</Text>
+          <View>
+            <MaterialIcons name="event" size={20} color="black" />
+            <Text>{item.dueDates[0].substring(0, 10)}</Text>
+          </View>
+        </View>
+        <View>
+          <Text>Recurrence</Text>
+          <Picker
+            selectedValue={item.recurrence}
+            onValueChange={handleRecurrenceChange}
+          >
+            <Picker.Item label="None" value="none" />
+            <Picker.Item label="Annually" value="annually" />
+            <Picker.Item label="Monthly" value="monthly" />
+          </Picker>
+        </View>
       </View>
-    );
-  }
+      <View>
+        <Text>Description</Text>
+        <TextInput
+          multiline
+          value={item.description}
+          onChangeText={(text) => {}}
+          style={styles.description}
+        />
+      </View>
+      <View>
+        <Text style={styles.label}>Assignee(s)</Text>
+        <View style={{ flexDirection: "row" }}>
+          {item.assignees.map((assignee) => (
+            <View key={assignee.id} style={{ alignItems: "center", marginRight: 10 }}>
+              <Image
+                source={require("../../assets/HD/character_48X48.png")}
+                style={{ width: 90, height: 90, marginBottom: 10 }}
+                resizeMode="stretch"
+              />
+              <Text style={styles.label}>{assignee.username}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </View>
+  );
 
   return (
     <View style={{ flex: 1, padding: 30 }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 35 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 35 }}>
+        <TouchableOpacity onPress={back}>
           <MaterialIcons name="keyboard-backspace" size={30} color="black" />
-          <TouchableOpacity onPress={toggleEditing}>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={toggleEditing}>
           <AntDesign name="form" size={25} color="black" />
         </TouchableOpacity>
       </View>
       <Text style={styles.header}>QuestBit Details</Text>
-      <View style={styles.section}>
-          <Text style={styles.label}>Quest</Text>
-          <Text style={styles.quest}>to do later</Text>
-        </View>
-      {isEditing ? (
-        <EditableQuestItem item={item} />
-      ) : (
-        renderQuestBitDetails()
-      )}
+      <View>
+        <Text style={styles.label}>Quest</Text>
+        <Text style={styles.title}>to do later</Text>
+      </View>
+      {isEditing ? <EditableQuestItem /> : renderQuestBitDetails()}
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   log: {
@@ -224,7 +181,7 @@ const styles = StyleSheet.create({
     width: "120%",
     height: "100%",
     marginBottom: 10,
-    marginTop: -70,
+    marginTop: 0,
   },
   character: {
     width: 90,
@@ -234,7 +191,7 @@ const styles = StyleSheet.create({
   title: {
     fontFamily: 'ZCOOL', 
     fontSize: 32,
-    marginTop: -8
+    marginTop: -8,
   },
   username: {
     fontFamily: "ZCOOL",
