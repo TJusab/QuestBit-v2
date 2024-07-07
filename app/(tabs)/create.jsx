8ae71b83cfe4b36cfe6039a60ede0dfe7973168a
James Luu <jamesluu0917@gmail.com>
2024-07-07 12:00:33 -0400

    redid page

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,148 +1,95 @@
-import React, { useState } from "react";
+import { router } from "expo-router";
 import {
   View,
   Text,
-  TextInput,
-  Button,
-  Switch,
-  Picker,
-  StyleSheet,
   TouchableOpacity,
+  TextInput,
+  StyleSheet,
   Image,
 } from "react-native";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import PixelButton from "../../components/PixelButton";
+import { useState } from "react";
 
 const Create = () => {
-  const [title, setTitle] = useState("");
-  const [dueDate, setDueDate] = useState(new Date());
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [isRecurring, setIsRecurring] = useState(false);
-  const [recurrenceOption, setRecurrenceOption] = useState("Annually");
-  const [description, setDescription] = useState("");
-  const [status, setStatus] = useState(false);
-
-  const handleDateChange = (event, selectedDate) => {
-    const currentDate = selectedDate || dueDate;
-    setShowDatePicker(false);
-    setDueDate(currentDate);
-  };
-
-  const handleSubmit = () => {
-    // Handle form submission
-    console.log({
-      title,
-      dueDate,
-      isRecurring,
-      recurrenceOption,
-      description,
-      status,
-    });
-  };
-
+  const [title, setTitle] = useState("Birthday Party Planning");
+  const [synopsis, setSynopsis] = useState(
+    "Plan a successful surprise birthday party for Bob."
+  );
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Title</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={setTitle}
-        placeholder="Title"
-      />
-
-      <Text style={styles.label}>Due Date</Text>
-      <TouchableOpacity onPress={() => setShowDatePicker(true)}>
-        <Text style={styles.input}>{dueDate.toDateString()}</Text>
-      </TouchableOpacity>
-      {showDatePicker && (
-        <DateTimePicker
-          value={dueDate}
-          mode="date"
-          display="default"
-          onChange={handleDateChange}
-        />
-      )}
-
-      <View style={styles.row}>
-        <Text style={styles.label}>Recurring QuestBit?</Text>
-        <Switch value={isRecurring} onValueChange={setIsRecurring} />
-      </View>
-
-      {isRecurring && (
-        <View>
-          <Text style={styles.label}>Recurrence Option</Text>
-          <Picker
-            selectedValue={recurrenceOption}
-            onValueChange={(itemValue) => setRecurrenceOption(itemValue)}
-            style={styles.input}
-          >
-            <Picker.Item label="Annually" value="Annually" />
-            <Picker.Item label="Monthly" value="Monthly" />
-            <Picker.Item label="Weekly" value="Weekly" />
-            <Picker.Item label="Daily" value="Daily" />
-          </Picker>
+    <View className="flex-1">
+      <View className="z-0 h-[40%] bg-white z-10">
+        <View className="w-full">
+          <View className="flex-row items-center justify-between px-4 mt-10">
+            <TouchableOpacity onPress={() => router.back()}>
+              <MaterialIcons
+                name="keyboard-backspace"
+                size={30}
+                color="black"
+              />
+            </TouchableOpacity>
+            <View className="flex-1 items-center">
+              <Text className="font-zcool text-3xl">Add Adventurers</Text>
+            </View>
+          </View>
         </View>
-      )}
-
-      <Text style={styles.label}>Description</Text>
-      <TextInput
-        style={[styles.input, styles.textArea]}
-        value={description}
-        onChangeText={setDescription}
-        placeholder="Description"
-        multiline
-      />
-
-      <View style={styles.row}>
-        <Text style={styles.label}>Status</Text>
-        <Switch value={status} onValueChange={setStatus} />
       </View>
-
-      <Text style={styles.label}>Assignee(s)</Text>
-      <View style={styles.assigneeContainer}>
-        <Image
-          source={{ uri: "https://via.placeholder.com/50" }} // Replace with your image URI
-          style={styles.assigneeImage}
-        />
-        <Text>Jxl_vu</Text>
+      <View className="z-10 flex-1 bg-blue-200 rounded-t-3xl -mt-5">
+        <Text className="text-3xl font-zcool text-white mt-10 text-center">
+          Create a Quest
+        </Text>
+        <View className="mx-5 mb-10">
+          <Text className="text-white font-zcool text-lg">
+            Title<Text className="text-red font-zcool text-lg">*</Text>
+          </Text>
+          <TextInput
+            style={styles.textInput}
+            value={title}
+            onChangeText={setTitle}
+            placeholderTextColor="white"
+            className="text-xl mb-5"
+          />
+          <Text className="text-white font-zcool text-lg">
+            Icon<Text className="text-red font-zcool text-lg">*</Text>
+          </Text>
+          <Image
+            source={require("../../assets/HD/chest.png")}
+            style={{ width: 48, height: 48 }}
+          />
+          <Text className="text-white font-zcool text-lg mt-5">Synopsis</Text>
+          <TextInput
+            style={styles.textInput}
+            value={synopsis}
+            onChangeText={setSynopsis}
+            placeholderTextColor="white"
+            className="text-xl"
+          />
+          <Text className="text-white font-zcool text-lg mt-5">
+            Target Date
+          </Text>
+          <View className="flex-row items-center mt-2">
+            <Icon name="clock" size={20} color="#FFF" />
+            <Text className="font-zcool text-white text-xl px-2">
+              January 4, 2024
+            </Text>
+          </View>
+        </View>
+        <PixelButton text="LAUNCH!" color="blue" />
       </View>
-
-      <Button title="Create QuestBit" onPress={handleSubmit} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-  },
-  label: {
-    fontSize: 16,
-    marginVertical: 8,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 8,
-    borderRadius: 4,
-  },
-  textArea: {
-    height: 80,
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginVertical: 8,
-  },
-  assigneeContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginVertical: 8,
-  },
-  assigneeImage: {
-    width: 50,
-    height: 50,
-    marginRight: 8,
+  textInput: {
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+    color: "white",
+    width: "100%",
+    fontFamily: "ZCOOL",
+    padding: 5,
   },
 });
 
