372f440eee6774451d29af7fd2d6e113bf2bde8a
James Luu <jamesluu0917@gmail.com>
2024-09-06 14:03:00 -0400

    Disabled recurrence picker when not recurring

--- app/pages/create_questbit.tsx
+++ app/pages/create_questbit.tsx
@@ -76,7 +76,7 @@ const Create = () => {
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const data = [{ key: '1' }]; // Dummy data for FlatList
+  const data = [{ key: "1" }]; // Dummy data for FlatList
 
   let newDueDates: Date[] = [];
 
@@ -302,15 +302,19 @@ const Create = () => {
   };
 
   // Updated function to handle either a boolean or a functional update
-  const handleQuestOpen = (open: boolean | ((prevOpen: boolean) => boolean)) => {
-    const isOpen = typeof open === 'function' ? open(questOpen) : open;
+  const handleQuestOpen = (
+    open: boolean | ((prevOpen: boolean) => boolean)
+  ) => {
+    const isOpen = typeof open === "function" ? open(questOpen) : open;
     setQuestOpen(isOpen);
     setIsDropdownOpen(isOpen); // Sync scroll-enabled state
   };
 
   // Updated function to handle either a boolean or a functional update
-  const handleRecurrOpen = (open: boolean | ((prevOpen: boolean) => boolean)) => {
-    const isOpen = typeof open === 'function' ? open(recurrOpen) : open;
+  const handleRecurrOpen = (
+    open: boolean | ((prevOpen: boolean) => boolean)
+  ) => {
+    const isOpen = typeof open === "function" ? open(recurrOpen) : open;
     setRecurrOpen(isOpen);
     setIsDropdownOpen(isOpen); // Sync scroll-enabled state
   };
@@ -319,213 +323,206 @@ const Create = () => {
     <FlatList
       data={data}
       renderItem={() => (
-    
-    <View className="flex-1">
-      <View className="z-0 bg-blue-200 z-10 mb-3">
-        <View className="w-full mt-5 mb-5">
-          <View className="flex-row items-center justify-between px-4 mt-10">
-            <TouchableOpacity onPress={() => router.back()}>
-              <MaterialIcons
-                name="keyboard-backspace"
-                size={30}
-                color="white"
-              />
-            </TouchableOpacity>
-            <View className="flex-1 items-center">
-              <View className="flex-row items-center">
-                <Text className="font-zcool text-3xl mr-5 text-white">
-                  Create a QuestBit
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View className="mx-7 mt-5 mb-10">
-            <Text className="text-white font-zcool text-lg">
-              Title<Text className="text-red font-zcool text-lg">*</Text>
-            </Text>
-            <TextInput
-              style={styles.textInput}
-              value={title}
-              onChangeText={setTitle}
-              placeholderTextColor="white"
-              className="text-xl mb-5"
-            />
-            <Text className="text-white font-zcool text-lg mt-5">
-              Due Date<Text className="text-red font-zcool text-lg">*</Text>
-            </Text>
-            <View>
-              <TouchableOpacity
-                onPress={() => setIsCalendarVisible(true)}
-                className="flex-row items-center mt-2"
-              >
-                <Icon name="clock" size={20} color="#FFF" />
-                <Text className="font-zcool text-white text-xl px-2">
-                  {formattedDate}
-                </Text>
-              </TouchableOpacity>
-              {/* <TouchableOpacity onPress={() => setShowDatePicker(true)}>
-                <Text style={styles.textInput}>
-                  {dueDate ? dueDate.toDateString() : ""}
-                </Text>
-              </TouchableOpacity>
-              {showDatePicker && (
-                <DateTimePicker
-                  value={dueDate || new Date()}
-                  mode="date"
-                  display="default"
-                  onChange={handleDateChange}
-                />
-              )} */}
-            </View>
-          </View>
-        </View>
-      </View>
-      <View className="z-10 shadow-xl flex-1 bg-white rounded-t-2xl -mt-10">
-        <View className="mx-7 mb-10 mt-7">
-          <Text className="text-gray-100 font-zcool text-lg">
-            Quest
-            <Text className="text-red font-zcool text-lg">*</Text>
-          </Text>
-          <View className="mt-3 mb-5">
-            <DropDownPicker
-              open={questOpen}
-              value={quest}
-              items={questDropdown}
-              setOpen={handleQuestOpen} // Call synchronized function
-              setValue={setQuest}
-              placeholder="Select quest"
-              style={styles.dropdown}
-              dropDownContainerStyle={styles.dropdown}
-              textStyle={styles.labelStyle}
-            />
-          </View>
-          <Divider color="black" />
-          <View className="flex-row justify-between items-center mt-5 mb-5">
-            <View className="flex flex-col space-y-2">
-              <Text className="text-black font-zcool text-lg text-gray-100">
-                Recurring QuestBit?
-              </Text>
-              <CheckBox
-                disabled={false}
-                value={isRecurring}
-                onValueChange={(newValue) => setIsRecurring(newValue)}
-                style={[styles.checkbox, isRecurring && styles.checkboxChecked]}
-              />
-            </View>
-            <View className="flex flex-col space-y-2">
-              <DropDownPicker
-                open={recurrOpen}
-                value={recurrenceOption}
-                items={recurrenceOptions}
-                setOpen={handleRecurrOpen}
-                setValue={setRecurrenceOption}
-                onChangeValue={(value) => {}}
-                placeholder="Select recurrence option"
-                style={styles.dropdown}
-                dropDownContainerStyle={styles.dropdown}
-                textStyle={styles.labelStyle}
-              />
-            </View>
-          </View>
-          <Divider color="black" />
-          <Text className="text-gray-100 font-zcool text-lg mt-3">
-            Description
-            <Text className="text-red font-zcool text-lg">*</Text>
-          </Text>
-          <TextInput
-            editable
-            multiline
-            numberOfLines={4}
-            maxLength={100}
-            style={styles.textInput2}
-            value={description}
-            onChangeText={setDescription}
-            placeholderTextColor="black"
-            className="text-xl mb-5"
-          />
-          <View className="flex-row">
-            <View className="flex-col mr-7">
-              <Text className="text-gray-100 font-zcool text-lg">Status</Text>
-              <View className="items-start">
-                <StatusButton
-                  color={getColorFromStatus(status)}
-                  text={status}
-                  textStyle="text-sm"
-                />
-              </View>
-            </View>
-
-            <View className="flex-col">
-              <Text className="text-gray-100 font-zcool text-lg">
-                Difficulty
-              </Text>
-              <View className="items-start">
-                <DifficultyButton
-                  color={getColorFromDifficulty(difficulty)}
-                  text={
-                    difficulty +
-                    "  |  " +
-                    getPointsFromDifficulty(difficulty) +
-                    " XP"
-                  }
-                  textStyle="text-sm"
-                />
-              </View>
-            </View>
-          </View>
-          <View className="flex-row mt-5">
-            <Text className="text-gray-100 font-zcool text-lg">Assignees</Text>
-            <TouchableOpacity onPress={() => setVisible(true)}>
-              <AntDesign name="pluscircle" size={25} color="green" />
-            </TouchableOpacity>
-            <AddPeopleModal
-              visible={visible}
-              onClose={() => setVisible(false)}
-              onUpdate={handleAddAdventurers}
-              selectedAdventurers={selectedAdventurers}
-              refreshKey={refreshKey}
-              text="Assign adventurers to this QuestBit"
-            />
-          </View>
-
-          <View className="items-start grid grid-col-3 gap-4">
-            {selectedAdventurers &&
-              selectedAdventurers.map((assignee) => (
-                <View
-                  key={assignee.$id}
-                  style={{ alignItems: "center", marginRight: 10 }}
-                >
-                  <Image
-                    source={getUserBodyIcon(assignee.icon)}
-                    style={styles.assigneeNames}
+        <View className="flex-1">
+          <View className="z-0 bg-blue-200 z-10 mb-3">
+            <View className="w-full mt-5 mb-5">
+              <View className="flex-row items-center justify-between px-4 mt-10">
+                <TouchableOpacity onPress={() => router.back()}>
+                  <MaterialIcons
+                    name="keyboard-backspace"
+                    size={30}
+                    color="white"
                   />
-                  <TouchableOpacity style={styles.remove_assignee}>
-                    <AntDesign name="minuscircle" size={25} color="red" />
-                  </TouchableOpacity>
-                  <Text>{assignee.username}</Text>
+                </TouchableOpacity>
+                <View className="flex-1 items-center">
+                  <View className="flex-row items-center">
+                    <Text className="font-zcool text-3xl mr-5 text-white">
+                      Create a QuestBit
+                    </Text>
+                  </View>
                 </View>
-              ))}
+              </View>
+              <View className="mx-7 mt-5 mb-10">
+                <Text className="text-white font-zcool text-lg">
+                  Title<Text className="text-red font-zcool text-lg">*</Text>
+                </Text>
+                <TextInput
+                  style={styles.textInput}
+                  value={title}
+                  onChangeText={setTitle}
+                  placeholderTextColor="white"
+                  className="text-xl mb-5"
+                />
+                <Text className="text-white font-zcool text-lg mt-5">
+                  Due Date<Text className="text-red font-zcool text-lg">*</Text>
+                </Text>
+                <View>
+                  <TouchableOpacity
+                    onPress={() => setIsCalendarVisible(true)}
+                    className="flex-row items-center mt-2"
+                  >
+                    <Icon name="clock" size={20} color="#FFF" />
+                    <Text className="font-zcool text-white text-xl px-2">
+                      {formattedDate}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
           </View>
-        </View>
+          <View className="z-10 shadow-xl flex-1 bg-white rounded-t-2xl -mt-10">
+            <View className="mx-7 mb-10 mt-7">
+              <Text className="text-gray-100 font-zcool text-lg">
+                Quest
+                <Text className="text-red font-zcool text-lg">*</Text>
+              </Text>
+              <View className="mt-3 mb-5">
+                <DropDownPicker
+                  open={questOpen}
+                  value={quest}
+                  items={questDropdown}
+                  setOpen={handleQuestOpen} // Call synchronized function
+                  setValue={setQuest}
+                  placeholder="Select quest"
+                  style={styles.dropdown}
+                  dropDownContainerStyle={styles.dropdown}
+                  textStyle={styles.labelStyle}
+                />
+              </View>
+              <Divider color="black" />
+              <View className="flex-row justify-between items-center mt-5 mb-5">
+                <View className="flex flex-col space-y-2">
+                  <Text className="text-black font-zcool text-lg text-gray-100">
+                    Recurring QuestBit?
+                  </Text>
+                  <CheckBox
+                    disabled={false}
+                    value={isRecurring}
+                    onValueChange={(newValue) => setIsRecurring(newValue)}
+                    style={[
+                      styles.checkbox,
+                      isRecurring && styles.checkboxChecked,
+                    ]}
+                  />
+                </View>
+                <View className="flex flex-col space-y-2">
+                  <DropDownPicker
+                    open={recurrOpen}
+                    value={recurrenceOption}
+                    items={recurrenceOptions}
+                    setOpen={handleRecurrOpen}
+                    setValue={setRecurrenceOption}
+                    onChangeValue={(value) => {}}
+                    placeholder="Select recurrence option"
+                    style={styles.dropdown}
+                    dropDownContainerStyle={styles.dropdown}
+                    textStyle={styles.labelStyle}
+                    disabled={!isRecurring}
+                  />
+                </View>
+              </View>
+              <Divider color="black" />
+              <Text className="text-gray-100 font-zcool text-lg mt-3">
+                Description
+                <Text className="text-red font-zcool text-lg">*</Text>
+              </Text>
+              <TextInput
+                editable
+                multiline
+                numberOfLines={4}
+                maxLength={100}
+                style={styles.textInput2}
+                value={description}
+                onChangeText={setDescription}
+                placeholderTextColor="black"
+                className="text-xl mb-5"
+              />
+              <View className="flex-row">
+                <View className="flex-col mr-7">
+                  <Text className="text-gray-100 font-zcool text-lg">
+                    Status
+                  </Text>
+                  <View className="items-start">
+                    <StatusButton
+                      color={getColorFromStatus(status)}
+                      text={status}
+                      textStyle="text-sm"
+                    />
+                  </View>
+                </View>
 
-        <View className="mb-5">
-          <PixelButton
-            text="Create questBit!"
-            textStyle="text-sm"
-            onPress={handleAddQuestBit}
+                <View className="flex-col">
+                  <Text className="text-gray-100 font-zcool text-lg">
+                    Difficulty
+                  </Text>
+                  <View className="items-start">
+                    <DifficultyButton
+                      color={getColorFromDifficulty(difficulty)}
+                      text={
+                        difficulty +
+                        "  |  " +
+                        getPointsFromDifficulty(difficulty) +
+                        " XP"
+                      }
+                      textStyle="text-sm"
+                    />
+                  </View>
+                </View>
+              </View>
+              <View className="flex-row mt-5">
+                <Text className="text-gray-100 font-zcool text-lg">
+                  Assignees
+                </Text>
+                <TouchableOpacity onPress={() => setVisible(true)}>
+                  <AntDesign name="pluscircle" size={25} color="green" />
+                </TouchableOpacity>
+                <AddPeopleModal
+                  visible={visible}
+                  onClose={() => setVisible(false)}
+                  onUpdate={handleAddAdventurers}
+                  selectedAdventurers={selectedAdventurers}
+                  refreshKey={refreshKey}
+                  text="Assign adventurers to this QuestBit"
+                />
+              </View>
+
+              <View className="items-start grid grid-col-3 gap-4">
+                {selectedAdventurers &&
+                  selectedAdventurers.map((assignee) => (
+                    <View
+                      key={assignee.$id}
+                      style={{ alignItems: "center", marginRight: 10 }}
+                    >
+                      <Image
+                        source={getUserBodyIcon(assignee.icon)}
+                        style={styles.assigneeNames}
+                      />
+                      <TouchableOpacity style={styles.remove_assignee}>
+                        <AntDesign name="minuscircle" size={25} color="red" />
+                      </TouchableOpacity>
+                      <Text>{assignee.username}</Text>
+                    </View>
+                  ))}
+              </View>
+            </View>
+
+            <View className="mb-5">
+              <PixelButton
+                text="Create questBit!"
+                textStyle="text-sm"
+                onPress={handleAddQuestBit}
+              />
+            </View>
+          </View>
+          <CalendarModal
+            visible={isCalendarVisible}
+            onClose={() => setIsCalendarVisible(false)}
+            onUpdate={handleDateUpdate}
           />
         </View>
-      </View>
-      <CalendarModal
-        visible={isCalendarVisible}
-        onClose={() => setIsCalendarVisible(false)}
-        onUpdate={handleDateUpdate}
-      />
-    </View>
-    
-  )}
-  keyExtractor={(item) => item.key}
-/>
+      )}
+      keyExtractor={(item) => item.key}
+    />
   );
 };
 
